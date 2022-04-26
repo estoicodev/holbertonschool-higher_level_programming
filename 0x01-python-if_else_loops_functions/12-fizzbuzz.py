bb389b52168c732cdc2d114d232689d23d858fb2
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-04-26 18:33:05 +0000

    Fix typo in task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for n in range(1, 101):
         if n % 3 == 0 and n % 5 == 0:
-            print("Fizzbuzz", end=" ")
+            print("FizzBuzz", end=" ")
         elif n % 3 == 0:
             print("Fizz", end=" ")
         elif n % 5 == 0:
