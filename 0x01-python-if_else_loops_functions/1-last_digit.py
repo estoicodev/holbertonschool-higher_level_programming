7d3b878fa0bf7bd2f93326ecf46ef5a5f6e0b8ca
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-04-26 17:50:47 +0000

    Fix PEP8 style in task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+less_than_six = "and is less than 6 and not 0"
 if number % 10 == 0:
     print(f"Last digit of {number} is {number % 10} and is 0")
 elif number < 0:
-    print(f"Last digit of {number} is -{-number % 10} and is less than 6 and not 0")
+    print(f"Last digit of {number} is -{-number % 10} {less_than_six}")
 else:
     if number % 10 < 6:
-        print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {number % 10} {less_than_six}")
     else:
         print(f"Last digit of {number} is {number % 10} and is greater than 5")
