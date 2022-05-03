1fea828d06dc52027dff03fe46e079e01c61c20d
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-05-03 03:24:04 +0000

    v1 of task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_list = []
+    for i in range(len(my_list)):
+        new_list.append(my_list[i])
+
+    if idx < 0 or idx > len(new_list):
+        return new_list
+
+    for i in range(len(new_list)):
+        if i == idx:
+            new_list[i] = element
+            return new_list
