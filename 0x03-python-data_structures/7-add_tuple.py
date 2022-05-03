8b9413db0c4a2b3d8e225bce130acf31204a545c
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-05-03 03:24:18 +0000

    v1 of task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 1:
+        first_a = tuple_a[0]
+        second_a = 0
+    elif len(tuple_a) == 0:
+        first_a = 0
+        second_a = 0
+    else:
+        first_a = tuple_a[0]
+        second_a = tuple_a[1]
+
+    if len(tuple_b) == 1:
+        first_b = tuple_b[0]
+        second_b = 0
+    elif len(tuple_b) == 0:
+        first_b = 0
+        second_b = 0
+    else:
+        first_b = tuple_b[0]
+        second_b = tuple_b[1]
+
+    new_tuple = (first_a + first_b, second_a + second_b)
+
+    return new_tuple
