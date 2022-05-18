ef50b4b8afff0ff52a0d4a37aafdffb9e3b4e0c9
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-05-18 04:32:15 +0000

    v1 of task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""This module creates a class named Square"""
+
+
+class Square:
+    """A class named Square
+
+    Attributes:
+    attr1 (size): size of square
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+        size (int): size attribute for the instance
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates of the square based on the size
+        Returns:
+        int: Returns the area of the square
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Gets the size of the instance of class Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the instance of class Square"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Prints an square figure of the instance of class Square"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    if j == self.__size - 1:
+                        print("#")
+                    else:
+                        print("#", end="")
