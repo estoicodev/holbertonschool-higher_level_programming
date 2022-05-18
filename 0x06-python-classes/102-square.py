f2f916528287cbbec4d3825eecab81b54fdca7b6
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-05-18 16:44:09 +0000

    v1 of task 102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+"""This module creates a class named Square"""
+
+
+class Square:
+    """A class named Square
+
+    Attributes:
+    attr1 (size): size of square
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+        size (int): size attribute for the instance
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates of the square based on the size
+        Returns:
+        int: Returns the area of the square
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Gets the size of the instance of class Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the instance of class Square"""
+        if type(value) != int or type(value) != float:
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __lt__(self, other):
+        """
+        Args:
+        other (obj): other instance of class Square
+        Returns:
+        int: Boolean value. Returns True or False
+        """
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """
+        Args:
+        other (obj): other instance of class Square
+        Returns:
+        int: Boolean value. Returns True or False
+        """
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """
+        Args:
+        other (obj): other instance of class Square
+        Returns:
+        int: Boolean value. Returns True or False
+        """
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """
+        Args:
+        other (obj): other instance of class Square
+        Returns:
+        int: Boolean value. Returns True or False
+        """
+        return self.area() >= other.area()
+
+    def __eq__(self, other):
+        """
+        Args:
+        other (obj): other instance of class Square
+        Returns:
+        int: Boolean value. Returns True or False
+        """
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """
+        Args:
+        other (obj): other instance of class Square
+        Returns:
+        int: Boolean value. Returns True or False
+        """
+        return self.area() != other.area()
