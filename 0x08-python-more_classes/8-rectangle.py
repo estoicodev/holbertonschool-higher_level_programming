1cc1e9887473458abe9756490a215d63a4c412f8
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-05-24 03:00:34 +0000

    v1.2 of task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 """This module creates a class named Rectangle"""
 
 
 class Rectangle:
     """A class named Square
-
     Attributes:
     attr1 (width): width of square
     attr2 (height): height of square
@@ -12,6 +12,18 @@ class Rectangle:
 
     number_of_instances = 0
     print_symbol = "#"
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns the biggest rectangle based on the area"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
 
     def __init__(self, width=0, height=0):
         """
@@ -73,8 +85,6 @@ class Rectangle:
         Returns:
         int: Returns the perimeter of the rectangle
         """
-        if self.__width == 0 or self.__height == 0:
-            return 0
         return (2 * self.__width) + (2 * self.__height)
 
     def __str__(self):
@@ -97,15 +107,3 @@ class Rectangle:
         """Delete the instance of class Rectangle"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Returns the biggest rectangle based on the area"""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
