cf916366698ff6a55fecd1a06f1311992e4488f0
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-06-01 02:57:43 +0000

    Fix pycodestyle of task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,8 @@
 
 
 def append_write(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written
+    """Appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
     Args:
     filename (str): filename
     text (str): text to append
