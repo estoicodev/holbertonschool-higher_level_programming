5e819ca502fb260f9ce6b09c746adbf248562c51
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-06-01 02:51:42 +0000

    Fix pycodestyle of task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+0;10;1c#!/usr/bin/python3
 """This module defines the write_file function"""
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written
+    """Writes a string to a text file (UTF8) and returns the number of
+    characters written
     Args:
     filename (str): filename
     text (str): text to write
