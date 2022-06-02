cf5997e20b4c47f3415c50883a99d24f5a5c8c7e
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-06-02 02:37:16 +0000

    v1 of task 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module defines a MyInt class inherited to int class"""
+
+
+class MyInt(int):
+    def __eq__(self, other):
+        """
+        Args:
+        other (obj): other instance of class MyInt
+        Returns:
+        int: Boolean value. Returns True or False
+        """
+        return self.real != other.real
+
+    def __ne__(self, other):
+        """
+        Args:
+        other (obj): other instance of class MyInt
+        Returns:
+        int: Boolean value. Returns True or False
+        """
+        return self.real == other.real
