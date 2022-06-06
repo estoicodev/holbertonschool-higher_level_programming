f4f05a58d6c892c180d882a517adb0d4ad020b65
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-06-06 17:54:25 +0000

    Fix typo rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,16 +13,15 @@ class Rectangle(Base):
     height (int): Height of the instance
     x (int): x of the instance
     y (int): y of the instance
-        """
-    __nb_objects = 0
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes an instance of Rectangle class"""
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     def area(self):
         """Returns the area of the instance Rectangle class"""
