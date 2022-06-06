40ea97e3556bd6c129a1122c46b04bb8464e4a77
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-06-06 03:40:47 +0000

    v1 of task 13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -111,3 +111,11 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def to_dictionary(self):
+        olddict = self.__dict__
+        newdict = {}
+        for e in olddict:
+            key = e.replace("_Rectangle__", "")
+            newdict[key] = olddict[e]
+        return newdict
