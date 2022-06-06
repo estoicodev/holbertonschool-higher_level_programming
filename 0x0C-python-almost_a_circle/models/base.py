d791b80a6a8d8f4b90c5e85305e18782a4661fbb
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-06-06 04:21:26 +0000

    v1 of task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,13 @@
 """This module defines a Base class"""
 
 
+import json
+
+
 class Base:
-    """class Base"""
+    """class Base
+    Attributes:
+    id (int): Id of the instance"""
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -12,3 +17,26 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        str_json = json.dumps(list_dictionaries)
+        return str_json
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        list_dicts = []
+        if list_objs is None:
+            with open(f"{cls.__name__}.json", mode="w", encoding="utf-8") as f:
+                f.write("[]")
+            return
+        for instance in list_objs:
+            new_dict = instance.to_dictionary()
+            list_dicts.append(new_dict)
+        with open(f"{cls.__name__}.json", mode="w", encoding="utf-8") as f:
+            text = Base.to_json_string(list_dicts)
+            f.write(text)
