19d6dcfc524d1c5e3d000a941a311a50de663ece
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-06-07 20:04:28 +0000

    Add csv functions (advanced task 20)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,5 @@
 """This module defines a Base class"""
-
-
+import csv
 import json
 from os import path
 
@@ -11,6 +10,7 @@ class Base:
     id (int): Id of the instance"""
     __nb_objects = 0
 
+    @staticmethod
     def reset_class():
         """Reset the attributes of the class for testing"""
         Base.__nb_objects = 0
@@ -40,7 +40,7 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Writes the JSON string representation of list_objs to a file"""
+        """Writes the JSON string representation of list_objs to a json file"""
         list_dicts = []
         if list_objs is None or len(list_objs) == 0:
             with open(f"{cls.__name__}.json", mode="w", encoding="utf-8") as f:
@@ -52,6 +52,35 @@ class Base:
             list_dicts.append(new_dict)
         with open(f"{cls.__name__}.json", mode="w", encoding="utf-8") as f:
             text = Base.to_json_string(list_dicts)
+            f.write(text)
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Writes the csv string representation of list_objs to a csv file"""
+        list_dicts = []
+        if list_objs is None or len(list_objs) == 0:
+            with open(f"{cls.__name__}.csv", mode="w", encoding="utf-8") as f:
+                f.write("")
+            return
+        for instance in list_objs:
+            new_dict = instance.to_dictionary()
+            list_dicts.append(new_dict)
+        with open(f"{cls.__name__}.csv", mode="w", encoding="utf-8") as f:
+            if str(cls.__name__) == "Rectangle":
+                keys = ["id", "width", "height", "x", "y"]
+            elif str(cls.__name__) == "Square":
+                keys = ["id", "size", "x", "y"]
+            text = ""
+            for i in range(len(list_dicts)):
+                obj = list_dicts[i]
+                for j in range(len(obj)):
+                    key = keys[j]
+                    if j != len(obj) - 1:
+                        text += f"{obj[key]},"
+                    else:
+                        text += f"{obj[key]}"
+                if i != len(list_dicts) - 1:
+                    text += "\n"
             f.write(text)
 
     @classmethod
@@ -68,7 +97,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """Returns a list of instances"""
+        """Returns a list of instances from a json file"""
         if not path.exists(f"{cls.__name__}.json"):
             return []
         with open(f"{cls.__name__}.json", encoding="utf-8") as f:
@@ -79,4 +108,23 @@ class Base:
         for instance in obj_list:
             new_instance = cls.create(**instance)
             list_instances.append(new_instance)
+        return list_instances
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Returns a list of instances from a csv file"""
+        if not path.exists(f"{cls.__name__}.csv"):
+            return []
+        with open(f"{cls.__name__}.csv", encoding="utf-8") as f:
+            if str(cls.__name__) == "Rectangle":
+                keys = ["id", "width", "height", "x", "y"]
+            elif str(cls.__name__) == "Square":
+                keys = ["id", "size", "x", "y"]
+            reader = csv.DictReader(f, fieldnames=keys)
+            list_instances = []
+            for dict_instance in reader:
+                for key, value in dict_instance.items():
+                    dict_instance[key] = int(value)
+                new_instance = cls.create(**dict_instance)
+                list_instances.append(new_instance)
         return list_instances
