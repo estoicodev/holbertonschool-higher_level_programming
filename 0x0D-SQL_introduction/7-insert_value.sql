f12001ce8d7df9422a646f908bc3a3494fd51df2
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-07-05 20:54:47 -0500

    v1 of task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- lists all rows of the table first_table from the database hbtn_0c_0
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (90, "Other School");
