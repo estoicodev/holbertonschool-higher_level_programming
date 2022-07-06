1d80c254896795c89457e045b6b841ebec5b3095
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-07-06 13:58:27 -0500

    v1.2 of task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,7 +3,7 @@
 -- Results must be sorted in ascending order
 -- by tv_shows.title and tv_show_genres.genre_id
 SELECT tvs.title, tvs_g.genre_id
-FROM tv_shows AS tvs FULL OUTER JOIN tv_show_genres AS tvs_g
+FROM tv_shows AS tvs LEFT JOIN tv_show_genres AS tvs_g
 ON tvs.id = tvs_g.show_id
 WHERE tvs_g.show_id IS NULL
 ORDER BY tvs.title ASC, tvs_g.genre_id ASC;
