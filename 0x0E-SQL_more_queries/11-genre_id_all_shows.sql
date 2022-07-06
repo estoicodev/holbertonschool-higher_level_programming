42f6b32ca1a646bd3e2a2edcc24e72c96dd9145f
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-07-06 13:47:05 -0500

    v1.1 of task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,6 +4,6 @@
 -- tv_shows.title and tv_show_genres.genre_id
 -- If a show doesn’t have a genre, display NULL
 SELECT tvs.title, tvs_g.genre_id
-FROM tv_shows AS tvs RIGHT JOIN tv_show_genres AS tvs_g
+FROM tv_shows AS tvs LEFT JOIN tv_show_genres AS tvs_g
 ON tvs.id = tvs_g.show_id
 ORDER BY tvs.title ASC, tvs_g.genre_id ASC;
