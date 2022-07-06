b932512f78d121599a39f12eab39d79051db9296
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-07-06 14:11:17 -0500

    v1.3 of task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,7 +6,7 @@
 -- Don’t display a genre that doesn’t have any shows linked
 -- Results must be sorted in descending order by
 -- the number of shows linked
-SELECT tvs_g.name AS genre, COUNT(tvs_g.show_id) AS number_of_shows
-FROM tv_show_genres AS tvs_g INNER JOIN tv_shows AS tvs
+SELECT tvg.name AS genre, COUNT(tvs_g.show_id) AS number_of_shows
+FROM tv_show_genres AS tvs_g INNER JOIN tv_genres AS tvg
 ON tvs_g.show_id = tvs.id
 ORDER BY number_of_shows DESC;
