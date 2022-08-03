dced0f52fff3fb0087d7cf38005c1f6d9a016758
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-08-02 23:47:38 -0500

    Add first line shebang in task 101

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,6 @@
-const functions = {
-  callMeMoby: function (x, theFunction) {
-    for (let i = 0; i < x; i++) {
-      theFunction();
-    }
+#!/usr/bin/node
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
   }
 };
-
-module.exports = functions;
