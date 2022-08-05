09e507f1ef62617dd14a6082ab1044e963380422
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-08-05 15:24:55 -0500

    v1 of task model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""State Class implementation"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class table"""
+
+    __tablename__ = 'states'
+
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String(128), nullable=False)
