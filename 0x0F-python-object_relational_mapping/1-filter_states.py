f4edca3b5dfa3c30136d759097f77a61e7a808e9
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-08-04 22:45:37 -0500

    v1 of task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module lists all 'states' from a database
+through command line
+"""
+from sys import argv as av
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", user=av[1], passwd=av[2], db=av[3])
+cur = db.cursor()
+cur.execute("SELECT * FROM hbtn_0e_0_usa.states\
+            WHERE states.name\
+            LIKE 'N%' ORDER BY states.id")
+rows = cur.fetchall()
+for row in rows:
+    print(f"{row}")
