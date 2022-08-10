490afde48cda4ba92998b7a931f72ef0d5482386
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-08-10 10:03:14 -0500

    v2 of task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,16 @@
 """This module lists all 'states' from a database
 through command line
 """
+
 from sys import argv as av
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user=av[1], passwd=av[2], db=av[3])
-cur = db.cursor()
-cur.execute("SELECT * from hbtn_0e_0_usa.states ORDER BY states.id")
-rows = cur.fetchall()
-for row in rows:
-    print(f"{row}")
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=av[1], passwd=av[2], db=av[3])
+    cur = db.cursor()
+    cur.execute("SELECT * from hbtn_0e_0_usa.states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"{row}")
+    cur.close()
+    db.close()
