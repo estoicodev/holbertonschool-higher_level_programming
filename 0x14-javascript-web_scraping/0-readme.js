2e6f4ad2c3bff175276f20e15f0ae49556d61af7
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-09-14 19:21:41 -0500

    v1.1 of task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-function readData (err, data) {
-  if (data) {
-    console.log(data);
-  } else {
-    console.log(err);
-  }
-}
-
-fs.readFile(process.argv[2], 'utf8', readData);
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  console.log(err || data);
+});
