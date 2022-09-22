cce2bf3a8d94e5c830055e1dc354dc0bbc850d18
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-09-22 00:14:11 -0500

    fix semistandard in task 8 and v1 of task 9

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
-$.get("https://swapi-api.hbtn.io/api/films/?format=json", function (data) {
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
   for (let i = 0; i < data.count; i++) {
-    $("UL#list_movies").append(`<li>${data.results[i]["title"]}</li>`);
+    $('UL#list_movies').append(`<li>${data.results[i].title}</li>`);
   }
 });
