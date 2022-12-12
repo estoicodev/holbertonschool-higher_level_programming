20149c764898f5873c58084b5b2e78ba4b53097b
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-12 14:02:33 -0500

    Deletes newline in concatenation task 102 (v2)

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 
 const data1 = fs.readFileSync(argv[2], 'utf8');
 const data2 = fs.readFileSync(argv[3], 'utf8');
-const dataConcat = data1.concat('\n' + data2);
+const dataConcat = data1.concat(data2);
 
 fs.writeFileSync(argv[4], dataConcat);
