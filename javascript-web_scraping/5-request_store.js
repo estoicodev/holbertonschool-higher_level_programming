a1343cd13e6ea0a95d2f5415165eff6b8b1940b3
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-12 16:55:50 -0500

    Task 5 (v1)

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (err, res, body) {
+  if (err) {
+    console.error('error:', err);
+  }
+  fs.writeFile(process.argv[3], body, 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
