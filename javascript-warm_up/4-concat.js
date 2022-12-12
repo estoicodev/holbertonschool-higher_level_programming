34cb59ed87b3b341ea355607c578c576fff41d78
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-12 11:58:57 -0500

    Task 4 completed (v1)

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const process = require('process');
+
+if (process.argv.length === 2) {
+  console.log('undefined is undefined');
+} else if (process.argv.length === 3) {
+  console.log(`${process.argv[2]} is undefined`);
+} else if (process.argv.length === 4) {
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
+}
