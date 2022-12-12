b8062f0a51110780b1d118954f86d0e80080828a
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-12 13:59:52 -0500

    Advanced task 102 (v1) that creates a file that concats 2 files. Using with console arguments

---
+++ javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+const fs = require('fs');
+
+const data1 = fs.readFileSync(argv[2], 'utf8');
+const data2 = fs.readFileSync(argv[3], 'utf8');
+const dataConcat = data1.concat('\n' + data2);
+
+fs.writeFileSync(argv[4], dataConcat);
