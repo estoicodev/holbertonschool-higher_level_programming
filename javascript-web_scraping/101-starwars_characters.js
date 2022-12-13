36c9a6852626fe5549f660deb423afe58f900d54
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-12 20:52:30 -0500

    Advanced task 101 completed with request module, new Promise() and Promise.all() to print all name's characters in order (v1)

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,36 +1,34 @@
 #!/usr/bin/node
 const request = require('request');
-const axios = require('axios');
 
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const URL_BASE = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, async function (err, res, body) {
+request(URL_BASE, function (err, res, body) {
   if (err) {
     console.error('error:', err);
+    return;
   }
   const characters = JSON.parse(body).characters;
-  const endpoints = [...characters];
-  axios
-    .all(endpoints.map(endpoint => axios.get(endpoint)))
-    .then(function (response) {
-      const responseKeys = Object.keys(response);
-      for (let i = 0; i < responseKeys.length; i++) {
-        console.log(response[i].data.name);
-      }
+  const promises = characters.map(function (characterUrl) {
+    return new Promise(function (resolve, reject) {
+      request(characterUrl, function (err, res, body) {
+        if (err) {
+          console.error('error:', err);
+          return;
+        }
+        const name = JSON.parse(body).name;
+        if (name) {
+          resolve(name);
+        } else {
+          reject(err);
+        }
+      });
+    });
+  });
+  Promise.all(promises)
+    .then(function (names) {
+      names.forEach(function (n) {
+        console.log(n);
+      });
     });
 });
-
-// request(url, async function (err, res, body) {
-//   if (err) {
-//     console.error('error:', err);
-//   }
-//   const characters = JSON.parse(body).characters;
-//   for (let i = 0; i < characters.length; i++) {
-//     request(characters[i], async function (err, res, body) {
-//         if (err) {
-//           console.error('error:', err);
-//         }
-//         console.log(JSON.parse(body).name);
-//     })
-//   }
-// });
